99fda91404928f9c626af5a5627ba5ad87d17613
kikonos2 <kikonos2@gmail.com>
2025-06-02 19:20:26 +0000

    Comparação

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -57,5 +57,45 @@ int main() {
         printf("Pontos turisticos: %d\n", cartas[i].pontos_turisticos);
     }
 
+    // Comparação: quem tem mais pontos turisticos vence
+    printf("\n=== Resultado do Duelo ===\n");
+    if (cartas[0].pontos_turisticos > cartas[1].pontos_turisticos) {
+        printf("A carta 1 vence com %d pontos turisticos!\n", cartas[0].pontos_turisticos);
+    } else if (cartas[1].pontos_turisticos > cartas[0].pontos_turisticos) {
+        printf("A carta 2 vence com %d pontos turisticos!\n", cartas[1].pontos_turisticos);
+    } else {
+        printf("Empate! Ambas as cartas têm %d pontos turisticos.\n", cartas[0].pontos_turisticos);
+    }
+
+    // Comparação: quem tem mais numero_habitantes vence
+printf("\n=== Resultado do Duelo (Número de Habitantes) ===\n");
+if (cartas[0].numero_habitantes > cartas[1].numero_habitantes) {
+    printf("A carta 1 vence com %d habitantes!\n", cartas[0].numero_habitantes);
+} else if (cartas[1].numero_habitantes > cartas[0].numero_habitantes) {
+    printf("A carta 2 vence com %d habitantes!\n", cartas[1].numero_habitantes);
+} else {
+    printf("Empate! Ambas as cartas têm %d habitantes.\n", cartas[0].numero_habitantes);
+}
+
+// Comparação: quem tem maior areaTotal vence
+printf("\n=== Resultado do Duelo (Área Total) ===\n");
+if (cartas[0].areaTotal > cartas[1].areaTotal) {
+    printf("A carta 1 vence com %.2f km² de área total!\n", cartas[0].areaTotal);
+} else if (cartas[1].areaTotal > cartas[0].areaTotal) {
+    printf("A carta 2 vence com %.2f km² de área total!\n", cartas[1].areaTotal);
+} else {
+    printf("Empate! Ambas as cartas têm %.2f km² de área total.\n", cartas[0].areaTotal);
+}
+
+// Comparação: quem tem maior pib vence
+printf("\n=== Resultado do Duelo (PIB) ===\n");
+if (cartas[0].pib > cartas[1].pib) {
+    printf("A carta 1 vence com R$ %.2f de PIB!\n", cartas[0].pib);
+} else if (cartas[1].pib > cartas[0].pib) {
+    printf("A carta 2 vence com R$ %.2f de PIB!\n", cartas[1].pib);
+} else {
+    printf("Empate! Ambas as cartas têm R$ %.2f de PIB.\n", cartas[0].pib);
+}
+
     return 0;
 }
